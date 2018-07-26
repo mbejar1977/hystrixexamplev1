b79f8f879febd0b9cfba97a2d75c56aa0d427ec2
marco <marcoabv77@gmail.com>
2018-07-26 18:02:03 -0500

    aaa

--- src/main/java/org/marco/ExampleService.java
+++ src/main/java/org/marco/ExampleService.java
@@ -15,14 +15,14 @@ public class ExampleService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ExampleService.class);
 
-	@HystrixCommand()
+	
 	public Response serviceA() {
 		LOGGER.warn("Header uuid: {}", DataContainer.getData().getUuid());
 		processThatCanFail();
 		return new Response("Response From Service A");
 	}
 
-	@HystrixCommand()
+	
 	public Response serviceB() {
 		LOGGER.warn("Header uuid: {}", DataContainer.getData().getUuid());
 		processThatCanDelay();
